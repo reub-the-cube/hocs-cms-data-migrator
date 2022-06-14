b66f0ee738f7c9b22816dca5ddba0010235785f6
sathik <sathik.abdulrahman@digital.homeoffice.gov.uk>
2022-06-14 19:15:15 +0100

    [HOCS-5145] Adding SQS connectivity with test messages.

--- src/main/java/uk/gov/digital/ho/hocs/cms/HocsCmsDataMigrator.java
+++ src/main/java/uk/gov/digital/ho/hocs/cms/HocsCmsDataMigrator.java
@@ -1,13 +1,7 @@
 package uk.gov.digital.ho.hocs.cms;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import uk.gov.digital.ho.hocs.client.MessageService;
-
-
 
 @SpringBootApplication
 public class HocsCmsDataMigrator {
