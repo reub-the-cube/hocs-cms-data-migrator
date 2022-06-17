d71bf65db541fc81d3f71906afa51670bab579e4
sathik <sathik.abdulrahman@digital.homeoffice.gov.uk>
2022-06-17 19:43:52 +0100

    [HOCS-5145]Addressed core review comments

---
+++ src/main/java/uk/gov/digital/ho/hocs/aws/sqs/AwsConfiguration.java
@@ -0,0 +1,46 @@
+package uk.gov.digital.ho.hocs.aws.sqs;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
+import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+@EnableSqs
+@Configuration
+@Profile("aws")
+@ConditionalOnProperty(prefix = "aws.sqs", value = "enabled", havingValue = "true")
+public class AwsConfiguration {
+
+    @Primary
+    @Bean
+    public AmazonSQSAsync awsSqsClient(@Value("${aws.sqs.access-key}") String accessKey,
+                                       @Value("${aws.sqs.secret-key}") String secretKey,
+                                       @Value("${aws.region}") String region) {
+
+        return AmazonSQSAsyncClientBuilder
+                .standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+                .build();
+    }
+
+
+    @Primary
+    @Bean
+    public SimpleMessageListenerContainerFactory simpleMessageListenerContainerFactory(AmazonSQSAsync amazonSqs) {
+        SimpleMessageListenerContainerFactory factory = new SimpleMessageListenerContainerFactory();
+
+        factory.setAmazonSqs(amazonSqs);
+        factory.setMaxNumberOfMessages(1);
+
+        return factory;
+    }
+}
--- src/main/java/uk/gov/digital/ho/hocs/cms/HocsCmsDataMigrator.java
+++ src/main/java/uk/gov/digital/ho/hocs/cms/HocsCmsDataMigrator.java
@@ -1,29 +1,19 @@
 package uk.gov.digital.ho.hocs.cms;
 
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import uk.gov.digital.ho.hocs.client.MessageService;
 
+
+
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.amazonaws.services.sqs", "uk.gov.digital.ho.hocs"})
-@Slf4j
-public class HocsCmsDataMigrator implements CommandLineRunner {
-
-	private final MessageService messageService;
-
-	public HocsCmsDataMigrator(MessageService messageService) {
-		this.messageService = messageService;
-	}
-
-	@Override
-	public void run(String... args) {
-		messageService.startSending();
-	}
+public class HocsCmsDataMigrator {
 
 	public static void main(String[] args) {
-		SpringApplication.run(uk.gov.digital.ho.hocs.cms.HocsCmsDataMigrator.class, args);
+		SpringApplication.run(HocsCmsDataMigrator.class, args);
 	}
+
 }
