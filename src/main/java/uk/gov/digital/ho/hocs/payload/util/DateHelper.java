d537e2a0df595d9fdd0af510f7e0d36babfcde0f
sathik <sathik.abdulrahman@digital.homeoffice.gov.uk>
2022-06-17 19:43:52 +0100

    [HOCS-5145]Addressed core review comments

---
+++ src/main/java/uk/gov/digital/ho/hocs/payload/util/DateHelper.java
@@ -0,0 +1,15 @@
+package uk.gov.digital.ho.hocs.payload.util;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class DateHelper {
+
+    public static LocalDate convertToLocalDate(Date date) {
+        return Instant.ofEpochMilli(date.getTime())
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+    }
+}
