7ab6091dd8ca09b7a683d5732429aa1599341c72
sathik <sathik.abdulrahman@digital.homeoffice.gov.uk>
2022-06-20 19:51:08 +0100

    [HOCS-5145]Added Unit tests

---
+++ src/test/java/uk/gov/digital/ho/hocs/client/MessageServiceTest.java
@@ -0,0 +1,33 @@
+package uk.gov.digital.ho.hocs.client;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class MessageServiceTest {
+
+	@Mock
+	private SQSClient sqsClient;
+
+	@Test
+	public void shouldSendCountMessagesWithComplaintType() {
+		int numMessages = 2;
+		MessageService messageService = new MessageService(sqsClient, numMessages, "BIOMETRIC");
+		messageService.startSending();
+		verify(sqsClient, times(numMessages)).sendMessage(anyString());
+	}
+
+	@Test
+	public void shouldSendCountMessagesWithoutComplaintType() {
+		int numMessages = 2;
+		MessageService messageService = new MessageService(sqsClient, numMessages, "");
+		messageService.startSending();
+		verify(sqsClient, times(numMessages)).sendMessage(anyString());
+	}
+}
