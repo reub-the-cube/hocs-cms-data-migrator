5f616eea5b1879978a21d1f45ced891bcc386218
Richard Weeks <richard.weeks1@digital.homeoffice.gov.uk>
2022-12-07 13:33:20 +0000

    HOCS-4831: Add TODO.

--- src/main/java/uk/gov/digital/ho/hocs/cms/complaints/ComplaintsService.java
+++ src/main/java/uk/gov/digital/ho/hocs/cms/complaints/ComplaintsService.java
@@ -10,7 +10,6 @@ import uk.gov.digital.ho.hocs.cms.domain.repository.DocumentsRepository;
 import uk.gov.digital.ho.hocs.cms.message.CaseAttachment;
 
 import java.math.BigDecimal;
-import java.sql.SQLException;
 import java.util.List;
 
 @Service
@@ -47,8 +46,6 @@ public class ComplaintsService {
         for (BigDecimal complaint : complaints) {
             extractComplaint(complaint.intValue());
         }
-        // TODO: Extract additional complaint data
-        // TODO: Check case record and build migration message
         log.info("Complaints extraction between dates {} and {} finished.", startDate, endDate);
     }
 
@@ -70,6 +67,9 @@ public class ComplaintsService {
     }
         cer.setStage("Documents");
         complaintsRepository.save(cer);
+
+        // TODO: Extract additional complaint data
+        // TODO: Check case record and build migration message
     }
 
 
