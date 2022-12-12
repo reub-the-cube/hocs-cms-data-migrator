8b0c27f79f80249d43c9172f7b242253d13de18a
Richard Weeks <richard.weeks1@digital.homeoffice.gov.uk>
2022-12-12 13:37:58 +0000

    HOCS-6012: Update to latest version mof migration message.

---
+++ src/main/java/uk/gov/digital/ho/hocs/cms/domain/message/Correspondent.java
@@ -0,0 +1,21 @@
+package uk.gov.digital.ho.hocs.cms.domain.message;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Correspondent {
+
+    private String fullName;
+    private String correspondentType;
+    private String address1;
+    private String address2;
+    private String address3;
+    private String postcode;
+    private String country;
+    private String organisation;
+    private String telephone;
+    private String email;
+    private String reference;
+}
