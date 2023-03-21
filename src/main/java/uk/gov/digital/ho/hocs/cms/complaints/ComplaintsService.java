2ed694c4d24b6538f15ce9b5e50c3011bd40d64d
Richard Weeks <richard.weeks1@digital.homeoffice.gov.uk>
2023-03-21 12:57:44 +0000

    Check for stage failures.

--- src/main/java/uk/gov/digital/ho/hocs/cms/complaints/ComplaintsService.java
+++ src/main/java/uk/gov/digital/ho/hocs/cms/complaints/ComplaintsService.java
@@ -210,23 +210,25 @@ public class ComplaintsService {
             }
         }
 
-        // populate message
-        if (sendMessage.equalsIgnoreCase("enabled")) {
-            CaseDetails message = complaintMessageBuilder.buildMessage(complaintId);
-            message.addCaseDataItems(decsCaseData.extractCaseData(complaintId));
-            message.setCaseAttachments(caseDetails.getCaseAttachments());
-            // send migration message
-            message.setSourceCaseId(complaintId.toString());
-            try {
-                messageService.sendMigrationMessage(message);
-                ComplaintExtractRecord correspondentStage = getComplaintExtractRecord(complaintId, "Migration message", true);
-                complaintsRepository.save(correspondentStage);
-            } catch (ApplicationExceptions.SendMigrationMessageException e) {
-                ComplaintExtractRecord correspondentStage = getComplaintExtractRecord(complaintId, "Migration message", false);
-                correspondentStage.setError(e.getEvent().toString());
-                correspondentStage.setErrorMessage(e.getMessage());
-                complaintsRepository.save(correspondentStage);
-                log.error("Failed sending migration message for complaint ID {}", complaintId + " skipping case...");
+        if (complaintsRepository.findStageFailureForCase(complaintId) == 0) {
+            // populate message
+            if (sendMessage.equalsIgnoreCase("enabled")) {
+                CaseDetails message = complaintMessageBuilder.buildMessage(complaintId);
+                message.addCaseDataItems(decsCaseData.extractCaseData(complaintId));
+                message.setCaseAttachments(caseDetails.getCaseAttachments());
+                // send migration message
+                message.setSourceCaseId(complaintId.toString());
+                try {
+                    messageService.sendMigrationMessage(message);
+                    ComplaintExtractRecord correspondentStage = getComplaintExtractRecord(complaintId, "Migration message", true);
+                    complaintsRepository.save(correspondentStage);
+                } catch (ApplicationExceptions.SendMigrationMessageException e) {
+                    ComplaintExtractRecord correspondentStage = getComplaintExtractRecord(complaintId, "Migration message", false);
+                    correspondentStage.setError(e.getEvent().toString());
+                    correspondentStage.setErrorMessage(e.getMessage());
+                    complaintsRepository.save(correspondentStage);
+                    log.error("Failed sending migration message for complaint ID {}", complaintId + " skipping case...");
+                }
             }
         }
     }
--- src/main/java/uk/gov/digital/ho/hocs/cms/domain/repository/ComplaintsRepository.java
+++ src/main/java/uk/gov/digital/ho/hocs/cms/domain/repository/ComplaintsRepository.java
@@ -1,9 +1,15 @@
 package uk.gov.digital.ho.hocs.cms.domain.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.digital.ho.hocs.cms.domain.model.ComplaintExtractRecord;
 
+import java.math.BigDecimal;
+
 @Repository
 public interface ComplaintsRepository extends CrudRepository<ComplaintExtractRecord, Long> {
+
+    @Query(value = "SELECT count(*) FROM complaints c WHERE c.complaint_extracted = false", nativeQuery = true)
+    int findStageFailureForCase(BigDecimal caseId);
 }
