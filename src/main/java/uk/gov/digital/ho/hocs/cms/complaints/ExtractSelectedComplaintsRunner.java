c2ceb53c592d6f76f1ec89d2d39f1cfc7c6c173a
sameer.srivastava <sameer.srivastava@digital.homeoffice.gov.uk>
2023-03-23 07:21:16 +0000

    HOCS-6248 : able to run the extractors

--- src/main/java/uk/gov/digital/ho/hocs/cms/complaints/ExtractSelectedComplaintsRunner.java
+++ src/main/java/uk/gov/digital/ho/hocs/cms/complaints/ExtractSelectedComplaintsRunner.java
@@ -8,28 +8,43 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 @Slf4j
-@ConditionalOnProperty(name = "cms.extract.single.complaint", havingValue = "enabled", matchIfMissing = false)
-public class ExtractSingleComplaintRunner implements CommandLineRunner {
+@ConditionalOnProperty(name = "cms.extract.multiple.complaints", havingValue = "enabled", matchIfMissing = false)
+public class ExtractSelectedComplaintsRunner implements CommandLineRunner {
 
     private final ApplicationContext applicationContext;
     private final ComplaintsService complaintsService;
 
-    private final String complaintId;
+    private final String complaintIds;
 
-    public ExtractSingleComplaintRunner(@Value("${complaint.id}") String complaintId,
+    public ExtractSelectedComplaintsRunner(@Value("${complaint.ids}") String complaintIds,
                                         ApplicationContext applicationContext,
                                         ComplaintsService complaintsService) {
-        this.complaintId = complaintId;
+        this.complaintIds = complaintIds;
         this.applicationContext = applicationContext;
         this.complaintsService = complaintsService;
     }
 
     @Override
     public void run(String... args) {
-        log.info("Extract a single complaint started for complaint ID {}", complaintId);
-        complaintsService.migrateComplaint(complaintId);
+        log.info("List of complaint ids from the environment {}", complaintIds);
+
+        List<String> complaintIdList = getComplaintIds(complaintIds);
+
+        for (String complaintId: complaintIdList) {
+            log.info("Extract a single complaint started for complaint ID {}", complaintId);
+            complaintsService.migrateComplaint(complaintId);
+        }
+
         System.exit(SpringApplication.exit(applicationContext, () -> 0));
     }
+
+    private List<String> getComplaintIds(String complaintIds){
+        return Arrays.asList(complaintIds.split("\\|"));
+    }
+
 }
