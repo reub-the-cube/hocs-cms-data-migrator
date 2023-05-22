dd8ab4b0653361fdc8d8181a26c9ceaceb3a82f8
Rishi Ramgolam <rishi.ramgolam@madetech.com>
2023-05-22 17:01:41 +0100

    HOCS-6626: Delete case before adding new (possibly updated) entry for correspondent table when creating a correspondent. Get individual objects for TO message builder.

--- src/main/java/uk/gov/digital/ho/hocs/cms/treatofficial/TreatOfficialCorrespondentExtractor.java
+++ src/main/java/uk/gov/digital/ho/hocs/cms/treatofficial/TreatOfficialCorrespondentExtractor.java
@@ -92,6 +92,7 @@ public class TreatOfficialCorrespondentExtractor {
             correspondentTreatOfficial.setCaseId(caseId);
             correspondentTreatOfficial.setIsPrimary(true);
             correspondentTreatOfficial.setCorrespondentId(individual.getPartyId());
+            treatOfficialCorrespondentsRepository.deleteAllByCaseId(caseId);
             treatOfficialCorrespondentsRepository.save(correspondentTreatOfficial);
         } catch (DataAccessException e) {
             log.error("Failed extracting correspondent details for complainant ID {} and case ID", correspondentId, caseId);
@@ -115,6 +116,7 @@ public class TreatOfficialCorrespondentExtractor {
                 correspondentTreatOfficial.setCaseId(caseId);
                 correspondentTreatOfficial.setIsPrimary(false);
                 correspondentTreatOfficial.setCorrespondentId(individual.getPartyId());
+                treatOfficialCorrespondentsRepository.deleteAllByCaseId(caseId);
                 treatOfficialCorrespondentsRepository.save(correspondentTreatOfficial);
             } catch (DataAccessException e) {
                 log.error("Failed extracting correspondent details for representative ID {} and case ID", correspondentId, caseId);
--- src/main/java/uk/gov/digital/ho/hocs/cms/treatofficial/TreatOfficialMessageBuilder.java
+++ src/main/java/uk/gov/digital/ho/hocs/cms/treatofficial/TreatOfficialMessageBuilder.java
@@ -2,6 +2,8 @@ package uk.gov.digital.ho.hocs.cms.treatofficial;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import uk.gov.digital.ho.hocs.cms.domain.exception.ApplicationExceptions;
+import uk.gov.digital.ho.hocs.cms.domain.exception.LogEvent;
 import uk.gov.digital.ho.hocs.cms.domain.message.CaseDetails;
 import uk.gov.digital.ho.hocs.cms.domain.model.CorrespondentTreatOfficial;
 import uk.gov.digital.ho.hocs.cms.domain.model.Individual;
@@ -31,10 +33,25 @@ public class TreatOfficialMessageBuilder {
         List<CorrespondentTreatOfficial> caseLink = treatOfficialCorrespondentsRepository.findByCaseId(caseId);
 
         Optional<Individual> primaryCorrespondent = getPrimaryCorrespondent(caseLink);
-        List<Optional<Individual>> representativeCorrespondent = getRepresentativeCorrespondents(caseLink);
+        List<Optional<Individual>> representativeCorrespondents = getRepresentativeCorrespondents(caseLink);
+
+        Individual individual = null;
+        individual = primaryCorrespondent.get();
+
+        List<Individual> representatives = getRepresentatives(representativeCorrespondents);
 
 
         return null;
+    }
+
+    public List<Individual> getRepresentatives(List<Optional<Individual>> representatives) {
+        List<Individual> individuals = new ArrayList<>();
+        for (Optional<Individual> rep : representatives) {
+            if (rep.isPresent()) {
+                individuals.add(rep.get());
+            }
+        }
+        return individuals;
     }
 
     private Optional<Individual> getPrimaryCorrespondent(List<CorrespondentTreatOfficial> caseLink) {
@@ -58,6 +75,12 @@ public class TreatOfficialMessageBuilder {
     }
 
     private Optional<Individual> getIndividual(BigDecimal correspondentId) {
+        Optional<Individual> individual = individualRepository.findById(correspondentId);
+        if (!individual.isPresent()) {
+            throw new ApplicationExceptions.SendMigrationMessageException(
+                    String.format("Correspondent doesn't exist. Complainant ID {}", correspondentId),
+                    LogEvent.MIGRATION_MESSAGE_FAILED);
+        }
         return individualRepository.findById(correspondentId);
     }
 
